a48c139b3454a2ee2b9c560e79326ffaeb5ec541
crepe-cell <crepe-yippee-attic@duck.com>
2025-06-15 13:44:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
-import geoip2.database
 import streamlit as st
+import geoip2.database
 
 def get_geolocation(ip_address):
-    # 使用 GeoLite2 数据库文件的路径
-    database_path = 'GeoLite2-City.mmdb'  # 替换为你的数据库文件路径
+    # 使用 GeoIP2 测试数据库文件的路径
+    database_path = 'MaxMind-DB/test-data/GeoIP2-City-Test.mmdb'  # 替换为你的数据库文件路径
     reader = geoip2.database.Reader(database_path)
     
     try:
@@ -36,6 +36,8 @@ def main():
                 st.write(f"纬度: {data['latitude']}")
                 st.write(f"经度: {data['longitude']}")
                 st.write(f"时区: {data['timezone']}")
+            else:
+                st.error("未找到该 IP 地址的信息。")
         else:
             st.warning("请输入有效的 IP 地址。")
 
